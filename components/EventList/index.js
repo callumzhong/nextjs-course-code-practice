7a391ee43ce58d7e850efe0cbf8540720e7bceb2
Callum Zhong <callum.zhong@gmail.com>
2022-11-08 17:14:07 +0800

    feat: add 74 section

--- components/EventList/index.js
+++ components/EventList/index.js
@@ -1,12 +1,18 @@
 import EventItem from './Item';
 
-export default function EventList(props) {
-  const { items } = props;
+export default function EventList({ items }) {
   return (
     <ul>
-      {items.map((item) => {
-        <EventItem />;
-      })}
+      {items.map((item) => (
+        <EventItem
+          key={item.id}
+          id={item.id}
+          date={item.date}
+          image={item.image}
+          location={item.location}
+          title={item.title}
+        />
+      ))}
     </ul>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import EventList from '../components/EventList';
+import { getFeaturedEvents } from '../data/dummy-data';
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+  console.log(
+    '🚀 ~ file: index.js ~ line 6 ~ HomePage ~ featuredEvents',
+    featuredEvents,
+  );
   return (
     <div>
-      <h1>The Home Page</h1>
+      <h1>sda</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
