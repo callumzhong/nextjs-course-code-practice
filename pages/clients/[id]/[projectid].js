86983ef5762d9a563e830069f8bc1aab0c131ad8
Callum Zhong <callum.zhong@gmail.com>
2022-11-08 12:55:57 +0800

    revert: 回到預設

--- pages/clients/[id]/[projectid].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-const ClientsProjectPage = () => {
-  const router = useRouter();
-  console.log(router.query);
-  return <div>The Clients Project Page</div>;
-};
-
-export default ClientsProjectPage;
